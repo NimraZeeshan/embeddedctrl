c3815c2d79bfccd0031f9e3032fe378625787273
brilianputraa <brilianpamiruddin@gmail.com>
2020-05-04 06:36:29 +0700

    Add reference signal (squarewave, sinusoidal, unit step, PRBS)

--- qlearningmotor.py
+++ qlearningmotor.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import time
 import random
+from scipy import signal
 # import Rpi.GPIO as GPIO
 
 # Parameter Init
@@ -13,21 +14,25 @@ destinationReward = 10
 epsilon = 0.95
 decayValue = epsilon / (episodeNum - 1)
 showPlot = 2500 # Will show plot every 2500 episode
+learningRate = 0.3
+discountRate = 0.9
 
 Q_table_start = np.zeros([128, 2]) # init the q table space, in this plant we have to action either to drive or brake
 
-learningRate = 0.3
-discountRate = 0.9
 setPoint = 32 # Choose your DC Motor Speed setpoint (in RPM)
 controlalgorithm = 'qlearn'
 currentError = setPoint
 error = np.array([43, 21, 122]) # array include three types of error [currenterror, derivativerror, sum of error]
-timeSampling = 0.001
+timeSampling = 0.001 # Systems sampling time
+samplingFreq = 1 / timeSampling # Systems sampling frequency 
 pwmResolution = 4096 #Because of raspberry pi 3 has 12bit resolution
 derivativeGain = 121
 proportionalGain = 1000
 integralGain = 0.02
-
+period = 0.5
+frequency = 1 / period
+amplitude = 32 # The range is from -maximum motor speed to maximum motor speed
+simulationTime = 360 # your simulation time
 # Main Program
 class main:
 	def __init__(self, pinPWM, setPoint, timeSampling):
@@ -45,6 +50,10 @@ class main:
 		self.epsilon = epsilon
 		self.episodeDecay = episodeDecay
 		self.episode = episodeNum
+		self.frequency = frequency
+		self.amplitude = amplitude
+		self.simulationTime = simulationTime
+
 	def control(self, controlalgorithm):
 		if controlalgorithm == 'PID':
 			controlout = self.controller.PID(self, error = error, sampling = timeSampling)
@@ -53,23 +62,54 @@ class main:
 		else:
 			controlout = self.controller.qlearn(self, error = error)
 		return controlout
+
 	def theAgent(self):
-		return error 
+		return error
+
+	def refSignal(self):
+		if self.frequency < 0.5 * samplingFreq:
+			raise Exception("Please use frequency at least less than half times of your sampling frequency to avoid aliasing")
+		t = np.linspace(0, self.simulationTime, self.simulationTime * 2)
+		# Signal Reference List		
+		if reference == 'sinusoidal': # Sinusoidal
+			y = np.sin(2 * np.pi * self.frequency * t)
+		elif reference == 'unit step': # Unit Step Signal
+			y = self.amplitude * np.heaviside(t, 0)
+		elif reference == 'square': # Square Wave Signal
+			y = signal.square(2 * np.pi * self.frequency * t)
+		elif reference == 'random'
+			# Pseudo Random Binary Sequence (PRBS)
+			extendTime = self.simulationTime * 10 # we multiple the simulation time by 10 to extend the PRBS signal time
+			initSpace = np.zeros(extendTime)
+			freqRange = np.random.randint(self.frequency, samplingFreq, extendTime)
+			idxAmp = 0
+			while idxAmp < extendTime:
+				initSpace[idxAmp] = self.amplitude
+				initSpace[idxAmp + 1] = -self.amplitude
+			 	idxAmp += 2
+			idxTime = 0
+			y = np.zeros(extendTime)
+			while freqRange[idxTime] < extendTime:
+				idx = freqRange[idxTime]
+				y[idx:] = initSpace[idxTime]
+				idxTime += 1
+		else: # Manual control
+			y = None
+		return y, t
+
 	class controller(main):
+		e = error[0]
+		edot = error[1]
+		esum = error[2]
+
 		def PID(self, error, sampling):
-			e = error[0]
-			edot = error[1]
-			esum = error[2]
 			controlout = self.proportionalGain * e + self.derivativeGain * edot / sampling + self.integralGain * esum * sampling
 			controlout = controlout / self.pwmResolution * 100
 			if controlout > self.pwmResolution:
 				controlout = 100
 			return controlout
+
 		def antiwindup(self, error, sampling):
-			controlout = PID(error, sampling)
-			e = error[0]
-			edot = error[1]
-			esum = error[2]
 			# Anti windup logic
 			if e <= 0.001: # when the error near to zero turn off the integral gain
 				controlout = self.proportionalGain * e + self.derivativeGain * edot / sampling 
@@ -77,7 +117,9 @@ class main:
 			else: # else act as normal PID 
 				controlout = self.controller.PID(error, sampling)
 			return controlout
+
 		def qlearn(self, error):
+			# The Q-Learning in this case will be act as a bang-bang controller (on-off control)
 			Q_table = Q_table_start
 			rowterm, colterm = 63, 1
 			init_state = ceil(error[0])
