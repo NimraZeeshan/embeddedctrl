4cecfc76a191192160e4e8b433d6913514b073e1
brilianputraa <brilianpamiruddin@gmail.com>
2020-05-06 02:03:26 +0700

    Add error update function and modification on class name

--- qlearningmotor.py
+++ qlearningmotor.py
@@ -19,10 +19,10 @@ discountRate = 0.9
 
 Q_table_start = np.zeros([128, 2]) # init the q table space, in this plant we have to action either to drive or brake
 
+initReading = 0 # The first time your sensor read the speed 
 setPoint = 32 # Choose your DC Motor Speed setpoint (in RPM)
 controlalgorithm = 'qlearn'
-currentError = setPoint
-error = np.array([43, 21, 122]) # array include three types of error [currenterror, derivativerror, sum of error]
+initError = np.array([setPoint - initReading, 0, 0]) # array include three types of error [currenterror, derivativerror, sum of error]
 timeSampling = 0.001 # Systems sampling time
 samplingFreq = 1 / timeSampling # Systems sampling frequency 
 pwmResolution = 4096 #Because of raspberry pi 3 has 12bit resolution
@@ -34,8 +34,9 @@ frequency = 1 / period
 amplitude = 32 # The range is from -maximum motor speed to maximum motor speed
 simulationTime = 360 # your simulation time
 # Main Program
-class main:
-	def __init__(self, pinPWM, setPoint, timeSampling):
+class env():
+
+	def __init__(self, pinPWM, lastError, timeSampling):
 		self.pinPWM = pinPWM
 		self.pinRotary = pinRotary
 		self.pwmResolution = pwmResolution
@@ -48,23 +49,25 @@ class main:
 		self.learningRate = learningRate
 		self.discountRate = discountRate
 		self.epsilon = epsilon
-		self.episodeDecay = episodeDecay
+		self.episodeDecay = decayValue
 		self.episode = episodeNum
 		self.frequency = frequency
 		self.amplitude = amplitude
 		self.simulationTime = simulationTime
+		self.lastError = lastError
 
-	def control(self, controlalgorithm):
-		if controlalgorithm == 'PID':
-			controlout = self.controller.PID(self, error = error, sampling = timeSampling)
-		elif controlalgorithm == 'antiwindup':
-			controlout = self.controller.antiwindup(self, error = error, sampling = timeSampling)
-		else:
-			controlout = self.controller.qlearn(self, error = error)
-		return controlout
+	def errorUpdate(self):
+		sensorRead = sensorRead()
+		new_state = ceil(sensorRead - self.setPoint) # this is the error at instance t+1
+		derror = self.lastError[1]
+		sumerror += self.lastError[2] * self.timeSampling
+		lastError = np.array([new_state, derror, sumerror])
+		self.lastError = lastError
+		return lastError
 
-	def theAgent(self):
-		return error
+	def sensorRead(self):
+		newval = random.randint(1, 100)
+		return newval
 
 	def refSignal(self):
 		if self.frequency < 0.5 * samplingFreq:
@@ -77,7 +80,7 @@ class main:
 			y = self.amplitude * np.heaviside(t, 0)
 		elif reference == 'square': # Square Wave Signal
 			y = signal.square(2 * np.pi * self.frequency * t)
-		elif reference == 'random'
+		elif reference == 'random':
 			# Pseudo Random Binary Sequence (PRBS)
 			extendTime = self.simulationTime * 10 # we multiple the simulation time by 10 to extend the PRBS signal time
 			initSpace = np.zeros(extendTime)
@@ -86,7 +89,7 @@ class main:
 			while idxAmp < extendTime:
 				initSpace[idxAmp] = self.amplitude
 				initSpace[idxAmp + 1] = -self.amplitude
-			 	idxAmp += 2
+				idxAmp += 2
 			idxTime = 0
 			y = np.zeros(extendTime)
 			while freqRange[idxTime] < extendTime:
@@ -97,59 +100,67 @@ class main:
 			y = None
 		return y, t
 
-	class controller(main):
-		e = error[0]
-		edot = error[1]
-		esum = error[2]
+	def PID(self, error, sampling):
+		controlout = self.proportionalGain * self.lastError[0] + self.derivativeGain * self.lastError[1] / sampling + self.integralGain * self.lastError[2]
+		controlout = controlout / self.pwmResolution * 100
+		if controlout > self.pwmResolution:
+			controlout = 100
+		return controlout
 
-		def PID(self, error, sampling):
-			controlout = self.proportionalGain * e + self.derivativeGain * edot / sampling + self.integralGain * esum * sampling
+	def antiwindup(self, error, sampling):
+		# Anti windup logic
+		if self.lastError[0] <= 0.001: # when the error near to zero turn off the integral gain
+			controlout = self.proportionalGain * self.lastError[0] + self.derivativeGain * self.lastError[1] / sampling 
 			controlout = controlout / self.pwmResolution * 100
-			if controlout > self.pwmResolution:
-				controlout = 100
-			return controlout
+		else: # else act as normal PID 
+			controlout = self.PID(error, sampling)
+		return controlout
 
-		def antiwindup(self, error, sampling):
-			# Anti windup logic
-			if e <= 0.001: # when the error near to zero turn off the integral gain
-				controlout = self.proportionalGain * e + self.derivativeGain * edot / sampling 
-				controlout = controlout / self.pwmResolution * 100
-			else: # else act as normal PID 
-				controlout = self.controller.PID(error, sampling)
-			return controlout
-
-		def qlearn(self, error):
-			# The Q-Learning in this case will be act as a bang-bang controller (on-off control)
-			Q_table = Q_table_start
-			rowterm, colterm = 63, 1
-			init_state = ceil(error[0])
-			for x in range(self.episode):
-				stop = False
-				while not stop:
-					if random.uniform(0, 1) < self.epsilon:
-						if random.uniform(0,1) > 0.5:
-							colstate = 1
-						else:
-							colstate = 0
-			   			controlout = Q_table[init_state, colstate]
+	def qlearn(self, error):
+		# The Q-Learning in this case will be act as a bang-bang controller (on-off control)
+		Q_table = Q_table_start
+		rowterm, colterm = 63, 1
+		init_state = ceil(error[0])
+		for x in range(self.episode):
+			stop = False
+			while not stop:
+				if random.uniform(0, 1) < self.epsilon:
+					if random.uniform(0,1) > 0.5:
+						colstate = 1
 					else:
-						colstate = np.argmax(Q_table[init_state, :])
+						colstate = 0
 						controlout = Q_table[init_state, colstate]
-					sensorRead = random.sample(range(1,64), 1) # Replace this with your raspberry pi 3 reading value
-					new_state = ceil(sensorRead - self.setPoint) # this is the new state
-					new_colstate = np.argmax(Q_table[new_state, :])
-					if (init_state == rowterm and colstate == colterm):
-						reward = destinationReward
-						stop = True
-					else:
-						reward = -rotatePenalty
-					now_qvalue = Q_table[init_state, :]
-					maxqnext = Q_table[new_state, new_colstate] 
-					new_qvalue = now_qvalue * (1 - learningRate) + learningRate * (reward + self.discountRate * maxqnext)
-					Q_table[init_state, colstate] = new_qvalue
-					init_state, colstate = new_state, new_colstate
-					if self.episodeNum >= episode >= 1:
-						self.epsilon -= decayValue
+				else:
+					colstate = np.argmax(Q_table[init_state, :])
+					controlout = Q_table[init_state, colstate]
+				new_state = self.errorUpdate(self.sensorRead()) # this is the new state
+				new_colstate = np.argmax(Q_table[new_state, :])
+				if (init_state == rowterm and colstate == colterm):
+					reward = destinationReward
+					stop = True
+				else:
+					reward = -rotatePenalty
+				now_qvalue = Q_table[init_state, :]
+				maxqnext = Q_table[new_state, new_colstate] 
+				new_qvalue = now_qvalue * (1 - learningRate) + learningRate * (reward + self.discountRate * maxqnext)
+				Q_table[init_state, colstate] = new_qvalue
+				init_state, colstate = new_state, new_colstate
+				if self.episodeNum >= episode >= 1:
+					self.epsilon -= decayValue
+
+class control(env):
+	
+	def __init__(self):
+		super().__init__(pinPWM, initError, timeSampling)
+	
+	def applications(self, controlalgorithm):
+		if controlalgorithm == 'PID':
+			controlout = super().PID(error = self.lastError, sampling = self.timeSampling)
+		elif controlalgorithm == 'antiwindup':
+			controlout = super().antiwindup(error = self.lastError, sampling = self.timeSampling)
+		else:
+			controlout = super().qlearn(error = self.lastError)
+		return controlout
 
 if __name__ == "__main__":
-    main()
+  	env(pinPWM, initError, timeSampling)
